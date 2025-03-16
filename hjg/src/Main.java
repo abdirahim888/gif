515ee5d564cc332b9b7f48640a8156d862c414c2
andirahim <burcifiqi@gamil.com>
2025-03-16 15:41:10 +0300

    commit #1
    in the nv on line 1 Defines the 'Main' class to test the implementation, on line 2 Main method where program execution begins, on line 3 Creates an instance of 'Book' with specific details, on line 4 Creates an instance of 'Laptop' with specific details.

---
+++ hjg/src/Main.java
@@ -0,0 +1,4 @@
+public class Main {
+    public static void main(String[] args) {
+        Book book1 = new Book("Java Programming", 101, "James Gosling", 450);
+        Laptop laptop1 = new Laptop("XPS 15", 201, "Dell", 3.2);
