8d12e008ab72f063eb723c25b53904cdfcc8351f
andirahim <burcifiqi@gamil.com>
2025-03-16 15:43:23 +0300

    commit #2
    in the nv on line  5 Calls the overridden method to display book details, on line 6 Calls the overridden method to display laptop details, on line 7 Calls the concrete method to compute and display a value for the book, on line 8 Calls the concrete method to compute and display a value for the laptop.

--- hjg/src/Main.java
+++ hjg/src/Main.java
@@ -2,3 +2,9 @@ public class Main {
     public static void main(String[] args) {
         Book book1 = new Book("Java Programming", 101, "James Gosling", 450);
         Laptop laptop1 = new Laptop("XPS 15", 201, "Dell", 3.2);
+        book1.displayDetails();
+        laptop1.displayDetails();
+        System.out.println("Computed Value for Book: " + book1.computeValue(10));
+        System.out.println("Computed Value for Laptop: " + laptop1.computeValue(20));
+    }
+}
