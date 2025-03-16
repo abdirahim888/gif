fce7b10e080f8adee778246684eff1d0b9c2c25e
andirahim <burcifiqi@gamil.com>
2025-03-16 15:17:55 +0300

    commit #1
    in the nv on line 1 Defines an abstract class as a base for different types of objects, on line 2 Declares a protected attribute 'name' to store the object's name, on line 3, Declares a protected attribute 'id' to store the object's unique identifier,  on line 4 Constructor initializes common attributes: 'name' and 'id' is added

---
+++ hjg/src/GeneralObject.java
@@ -0,0 +1,7 @@
+abstract class GeneralObject {
+    protected String name;
+    protected int id;
+    public GeneralObject(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
